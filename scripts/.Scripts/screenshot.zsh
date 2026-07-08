8b485057d936392174aea17fd49370d470e79deb
gr4b01d <Ialdabaot_archon@proton.me>
2026-07-08 21:44:07 +0100

    added a satty option

--- scripts/.Scripts/screenshot.zsh
+++ scripts/.Scripts/screenshot.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 #
-option=$(printf "Save Screen\nCopy Screen\nSave Region\nCopy Region" | fuzzel --dmenu -p "Screenshot" --lines=4)
+option=$(printf "Save Screen\nCopy Screen\nSave Region\nCopy Region\nAnnotate" | fuzzel --dmenu -p "Screenshot" --lines=5)
 
 case "$option" in
 	"Save Screen") hyprshot --output-folder /home/tagilla/Pictures/Hyprshot -m output -m active ;;
 	"Copy Screen") hyprshot --clipboard-only -m output -m active ;;
 	"Save Region") hyprshot --output-folder /home/tagilla/Pictures/Hyprshot -m region ;;
 	"Copy Region") hyprshot --clipboard-only -m region ;;
+  "Annotate") wl-paste | satty --filename - ;;
 esac
